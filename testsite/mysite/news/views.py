a4f942a0110dc9b4284f01dc950894c5980f1da6
matargon <erik.fr@yandex.ru>
2022-06-27 14:47:30 +0300

    27 использование в views классов

--- testsite/mysite/news/views.py
+++ testsite/mysite/news/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView
 from django.http import HttpResponse
 from .forms import NewsForm
 from .models import News, Category
 
+
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    # extra_context = {'title': 'Главная'} #используется только для константных значений
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(HomeNews, self).get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
 
 def index(request):
     news = News.objects.all()
