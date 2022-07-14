b2de579ba04262ff3551a4d91eaf2ad529dc0565
matargon <erik.fr@yandex.ru>
2022-07-14 19:57:16 +0300

    47 авторизация

--- testsite/mysite/news/forms.py
+++ testsite/mysite/news/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from .models import News
 import re
 from django.core.exceptions import ValidationError
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
@@ -19,6 +19,12 @@ from django.contrib.auth.models import User
 #     category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория ',
 #                                       empty_label='Выберите категорию',
 #                                       widget=forms.Select(attrs={"class": "form-control"}))
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label='Имя пользователя',
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 
 class UserRegisterForm(UserCreationForm):
--- testsite/mysite/news/views.py
+++ testsite/mysite/news/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import login, logout
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.http import HttpResponse
-from .forms import NewsForm, UserRegisterForm
+from .forms import NewsForm, UserRegisterForm, UserLoginForm
 from .models import News, Category
 from django.urls import reverse_lazy
 from .utils import MyMixin
@@ -16,9 +17,10 @@ def register(request):
         # form = UserCreationForm(request.POST)
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             messages.success(request, 'Вы успешно зарегестрировались')
-            return redirect('login')
+            return redirect('home')
         else:
             messages.error(request, 'Ошибка регистрация')
     else:
@@ -27,8 +29,21 @@ def register(request):
     return render(request, 'news/register.html', {'form': form})
 
 
-def login(request):
-    return render(request, 'news/login.html')
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'news/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
 
 
 def test(request):
