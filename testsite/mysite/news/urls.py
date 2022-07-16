a60c22b534a87be8984fb76fba2ab20a786f0e42
matargon <erik.fr@yandex.ru>
2022-07-16 14:45:05 +0300

    немного кеша

--- testsite/mysite/news/urls.py
+++ testsite/mysite/news/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import *
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('register/', register, name="register"),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     # path('', index, name='home'),
-    path('', HomeNews.as_view(), name='home'),
+    path('', cache_page(60 )(HomeNews.as_view()), name='home'),
     # path('category/<int:category_id>/', get_category, name='category'),
     path('category/<int:category_id>/', NewsByCategory.as_view(extra_context={'title': 'Какой-то тайтл'}),
          name='category'),
